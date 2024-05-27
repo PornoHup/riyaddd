4e82be1c8aee7cb24803bd816c2df6aa20d03bf4
PornoHup <107538335+PornoHup@users.noreply.github.com>
2024-05-27 12:07:35 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://www.instagram.com/p/CJORoF_DFEr/?igsh=emkzamEzdGJtazR1")
-START_IMG = getenv("START_IMG", "https://www.instagram.com/p/CJEBVFGDb3p/?igsh=MWUwc3F2aThrNDVrNw==")
+PING_IMG = getenv("PING_IMG", "https://images.app.goo.gl/gUqiKSRGJkcB16LDA")
+START_IMG = getenv("START_IMG", "https://images.app.goo.gl/2EnpdVRQ2fx531AD9")
 
 SESSION = getenv("SESSION", None)
 
