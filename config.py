80a28352e1e45077bd804a916f55ae46bd1cf619
PornoHup <107538335+PornoHup@users.noreply.github.com>
2024-05-27 10:54:11 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://images.app.goo.gl/yhGaMRSgTrGEZsEu8")
-START_IMG = getenv("START_IMG", "https://images.app.goo.gl/W4hSfXBDSs5empLr8")
+PING_IMG = getenv("PING_IMG", "https://www.instagram.com/p/CJORoF_DFEr/?igsh=emkzamEzdGJtazR1")
+START_IMG = getenv("START_IMG", "https://www.instagram.com/p/CJEBVFGDb3p/?igsh=MWUwc3F2aThrNDVrNw==")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RiyaddSupport")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RiyaddBlogg")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TuralBlogg")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NezrinBot")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5809546648").split()))
 
 
-FAILED = "https://images.app.goo.gl/W4hSfXBDSs5empLr8"
+FAILED = "https://www.instagram.com/p/CIvUqiXDGs5/?igsh=c25hZ2p3NnhrMDNr"
